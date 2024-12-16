cd0861441b93e61528c3ed3cbdaed3440f6b7bf7
Vlad <mun.vladislav.a@gmail.com>
2024-12-16 15:53:24 +0500

    filament package installed

--- lang/en/errors.php
+++ lang/en/errors.php
@@ -5,7 +5,9 @@ return [
         'access_token' => 'An error occurred while retrieving the firebase access token. Please try again',
         'notification' => [
             'statuses' => [
-                '400' => 'error'
+                '400' => 'Error',
+                '200' => 'Sended successfully',
+                '201' => 'Sended successfully'
             ]
         ]
     ]
